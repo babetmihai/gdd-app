47bb24dbad47bfd5154f78bff76a2493d328f63a
Mihai Babet <mihai.babet@gmail.com>
2020-04-24 20:58:07 +0300

    add autofocus

--- src/components/App/Questions/Textarea/index.jsx
+++ src/components/App/Questions/Textarea/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
+import styles from './index.module.scss'
+
 export default function Textarea({ onChange, value }) {
 
   return (
-    <Form.Group>
+    <Form.Group className={styles.textarea}>
       <Form.Control as="textarea"
+        autoFocus
         value={value}
         onChange={(event) => onChange(event.target.value)}
       />
