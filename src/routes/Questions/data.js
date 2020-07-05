df8817829a0272e19442bd31af888dd66f8193af
Mihai Babet <mihai.babet@gmail.com>
2020-07-05 21:05:00 +0300

    adjust css

--- src/routes/Questions/data.js
+++ src/routes/Questions/data.js
@@ -32,82 +32,61 @@ export default [
   { id: 'sports', options: ['team_sports', 'competitive'], requires: ['sports'] },
   { id: 'puzzle', options: ['logic', 'trivia'], requires: ['puzzle'] },
   { id: 'idle_game', options: ['casual', 'party_game', 'board_game', 'educational'], requires: ['puzzle'] },
+
+  { id: 'art_style', options: ['2d', '3d'] },
+
+  { id: '2d', options: ['pixel', 'vector', 'cutout', 'doodle'], requires: ['2d'] },
+  { id: '3d', options: ['realistic', 'cell_shaded', 'low_poly', 'sci_fi_realistic'], requires: ['3d'] },
+
+  { id: 'enviroment_design', options: ['custom_designed', 'procedural_generated'] },
+
+  { id: 'core_mechanics', options: ['dynamic', 'achievemets'] },
+  {
+    id: 'dynamic',
+    options: [
+      'racing',
+      'running',
+      'jumping',
+      'dodging',
+      'flying',
+      'attacking',
+      'syncronizing'
+    ],
+    requires: ['dynamic']
+  },
+  {
+    id: 'achievemets',
+    options: [
+      'collecting',
+      'crafting',
+      'questing',
+      'matching',
+      'exploring',
+      'developing'
+    ],
+    requires: ['achievemets']
+  },
+  { id: 'future_content', options: ['expansion', 'sequel', 'new_game'] },
+  {
+    id: 'expansion',
+    options: [
+      'new_quests',
+      'new_maps',
+      'new_items',
+      'new_features',
+      'new_activities'
+    ],
+    requires: ['expansion']
+  },
+  {
+    id: 'sequel',
+    options: [
+      'new_features_based_on_feedback',
+      'new_content_similar_to_the_previous_title'
+    ],
+    requires: ['sequel']
+  },
   { id: 'done', type: TYPES.DONE }
-
-  // { id: 'educational', requires: ['idle_game'] },
-
-  // { id: 'art_style', options: ['2d', '3d'], nextId: 'art_style_detail' },
-  // {
-  //   id: 'art_style_detail',
-  //   parentId: 'art_style',
-  //   nextId: 'core_mechanics',
-  //   options: ['pixel', 'vector', 'cutout', 'doodle', 'realistic', 'cell_shaded', 'low_poly', 'sci_fi_realistic']
-  // },
-
-  // { id: 'pixel', requires: ['2d'] },
-  // { id: 'vector', requires: ['2d'] },
-  // { id: 'cutout', requires: ['2d'] },
-  // { id: 'doodle', requires: ['2d'] },
-  // { id: 'realistic', requires: ['3d'] },
-  // { id: 'cell_shaded', requires: ['3d'] },
-  // { id: 'low_poly', requires: ['3d'] },
-  // { id: 'sci_fi_realistic', requires: ['3d'] },
-
-  // { id: 'core_mechanics', options: ['dynamic', 'achievemets'], nextId: 'core_mechanics_detail' },
-  // {
-  //   id: 'core_mechanics_detail',
-  //   parentId: 'core_mechanics',
-  //   nextId: 'future_content',
-  //   options: [
-  //     'racing',
-  //     'running',
-  //     'jumping',
-  //     'dodging',
-  //     'flying',
-  //     'attacking',
-  //     'syncronizing',
-  //     'collecting',
-  //     'crafting',
-  //     'questing',
-  //     'matching',
-  //     'exploring',
-  //     'developing'
-  //   ]
-  // },
-  // { id: 'racing', requires: ['dynamic'] },
-  // { id: 'running', requires: ['dynamic'] },
-  // { id: 'jumping', requires: ['dynamic'] },
-  // { id: 'dodging', requires: ['dynamic'] },
-  // { id: 'flying', requires: ['dynamic'] },
-  // { id: 'attacking', requires: ['dynamic'] },
-  // { id: 'syncronizing', requires: ['dynamic'] },
-  // { id: 'collecting', requires: ['achievemets'] },
-  // { id: 'crafting', requires: ['achievemets'] },
-  // { id: 'questing', requires: ['achievemets'] },
-  // { id: 'matching', requires: ['achievemets'] },
-  // { id: 'exploring', requires: ['achievemets'] },
-  // { id: 'developing', requires: ['achievemets'] },
-
-  // { id: 'future_content', options: ['expansion', 'sequel', 'new_game'], nextId: 'future_content_detail' },
-  // {
-  //   id: 'future_content_detail',
-  //   parentId: 'future_content',
-  //   nextId: 'done',
-  //   options: [
-  //     'new_quests',
-  //     'new_maps',
-  //     'new_items',
-  //     'new_features',
-  //     'new_content'
-  //   ]
-  // },
-
-  // { id: 'new_quests', requires: ['expansion'] },
-  // { id: 'new_maps', requires: ['expansion'] },
-  // { id: 'new_items', requires: ['expansion'] },
-  // { id: 'new_features', excludes: ['new_game'] },
-  // { id: 'new_content', excludes: ['expansion'] },
-
 ].reduce((acc, node) => {
   acc[node.id] = node
   return acc
