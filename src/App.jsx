0a5bcce59f747266d4ca836a4bb61a3a449d28b1
Mihai Babet <mihai.babet@gmail.com>
2020-08-01 20:28:22 +0300

    add layout component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 import Home from 'routes/Home'
+import Layout from 'core/layout/Layout'
 
 
 const routes = [
@@ -10,24 +11,27 @@ const routes = [
 
 export default function App() {
   return (
-    <Switch>
-      {routes.map((route) => {
-        const { id, path, component, icon, exact } = route
-        return (
-          <Route
-            key={id}
-            exact={exact}
-            path={path}
-            render={(props) => React.createElement(component, {
-              ...props,
-              id,
-              icon,
-              routes
-            })}
-          />
-        )
-      })}
-      <Redirect to="/" />
-    </Switch>
+    <Layout>
+      <Switch>
+        {routes.map((route) => {
+          const { id, path, component, icon, exact } = route
+          return (
+            <Route
+              key={id}
+              exact={exact}
+              path={path}
+              render={(props) => React.createElement(component, {
+                ...props,
+                id,
+                icon,
+                routes
+              })}
+            />
+          )
+        })}
+        <Redirect to="/" />
+      </Switch>
+    </Layout>
+
   )
 }
