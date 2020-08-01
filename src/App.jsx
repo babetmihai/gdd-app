ecc22d8608ca66f2cd8ec2ca697fa80691f359fc
Mihai Babet <mihai.babet@gmail.com>
2020-08-01 20:18:53 +0300

    add home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import Main from 'layout/Main'
+
+import Home from 'croutes/home'
 
 
 const routes = [
+  { id: 'home', path: '/', component: Home }
 ]
 
 export default function App() {
   return (
-    <Main routes={routes}>
-      <Switch>
-        {routes.map((route) => {
-          const { id, path, component, icon, exact } = route
-          return (
-            <Route
-              key={id}
-              exact={exact}
-              path={path}
-              render={(props) => React.createElement(component, {
-                ...props,
-                id,
-                icon,
-                routes
-              })}
-            />
-          )
-        })}
-        <Redirect to="/" />
-      </Switch>
-    </Main>
+    <Switch>
+      {routes.map((route) => {
+        const { id, path, component, icon, exact } = route
+        return (
+          <Route
+            key={id}
+            exact={exact}
+            path={path}
+            render={(props) => React.createElement(component, {
+              ...props,
+              id,
+              icon,
+              routes
+            })}
+          />
+        )
+      })}
+      <Redirect to="/" />
+    </Switch>
   )
 }
