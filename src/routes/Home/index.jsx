2f22827813eacd6c07e98c071211b1266800b378
Mihai Babet <mihai.babet@gmail.com>
2020-08-15 14:32:44 +0300

    css

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import styles from './index.module.scss'
 import { Typography, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <div>
       <Typography variant="h2">GDD Generator</Typography>
       <Typography variant="h4">Create yout Game Design Document</Typography>
       <Button
