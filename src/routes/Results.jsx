ddffc65f59e49c16e938a6b97000f34b3c598e50
Mihai Babet <mihai.babet@gmail.com>
2020-08-29 12:12:34 +0300

    add result page

--- src/routes/Results.jsx
+++ src/routes/Results.jsx
@@ -28,27 +28,45 @@ export default function Results() {
           flex: 'none'
         }}
       >
-        <h1>{t('place_in_game_culture')}</h1>
-        {results.pc &&
-          <>
-            <p>{t('pc_story')}</p>
-            <br />
-            {results.client && <p>{t('pc_client_story')}</p>}
-            {results.browser && <p>{t('pc_browser_story')}</p>}
-          </>
-        }
-        {results.console &&
-          <>
-            <p>{t('console_story')}</p>
-            <br />
-            {results.ps && <p>{t('console_ps_story')}</p>}
-            {results.xbox && <p>{t('console_xbox_story')}</p>}
-            {results.nintendo && <p>{t('console_nintendo_story')}</p>}
-          </>
-        }
-        <p>
-          {t('specify_culture')}
-        </p>
+        <h1>{t('game_design_document')}</h1>
+        <>
+          <h2>{t('place_in_game_culture')}</h2>
+          {results.pc &&
+            <>
+              <p>{t('pc_story')}</p>
+              <br />
+              {results.client && <p>{t('pc_client_story')}</p>}
+              {results.browser && <p>{t('pc_browser_story')}</p>}
+            </>
+          }
+          {results.console &&
+            <>
+              <p>{t('console_story')}</p>
+              <br />
+              {results.ps && <p>{t('console_ps_story')}</p>}
+              {results.xbox && <p>{t('console_xbox_story')}</p>}
+              {results.nintendo && <p>{t('console_nintendo_story')}</p>}
+            </>
+          }
+          <p>{t('culture_input_description')}</p>
+        </>
+        <>
+          <h2>{t('game_type')}</h2>
+          <p>{t('game_type_story')}</p>
+        </>
+        <br />
+        <>
+          <h2>{t('target_audience')}</h2>
+          <p>{t('target_audience_story')}</p>
+        </>
+        <>
+          <h2>{t('monetization')}</h2>
+          {results.free2play && <p>{t('free2play_story')}</p>}
+          {results.buy2play && <p>{t('buy2play_story')}</p>}
+          {results.subscription && <p>{t('subscription_story')}</p>}
+          <p>{t('monetization_input_description')}</p>
+        </>
+
       </div>
     </Card>
   )
